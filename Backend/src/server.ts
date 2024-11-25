f260ebf704e33d0beb41ab106878bd4403206cc7
Jephy Jayan Varghese <jephyjvarghese@gmail.com>
2024-11-25 21:58:06 +0530

    Update server.ts

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 export const io = new Server(server, {
     cors: {
         // origin: "http://localhost:5173",
-        origin: "https://trivandrum-scans.onrender.com",
+        origin: "https://trivandrum-scans-cq38.vercel.app/",
         methods: ["GET", "POST"],
         credentials: true,
     },
@@ -30,12 +30,13 @@ handleSocketEvents(io);
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
     // origin: ['http://localhost:5173'],
-    origin: ['https://trivandrum-scans.onrender.com'],
+    origin: ['https://trivandrum-scans-cq38.vercel.app/'],
 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', "PATCH"],
     allowedHeaders: ['Content-Type', 'Authorization'],
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
