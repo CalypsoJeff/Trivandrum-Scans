5a0fe426167f2e60f41b2517f90cff3f2c22c2b1
Jephy Jayan Varghese <jephyjvarghese@gmail.com>
2024-12-10 13:08:29 +0530

    s3 bucket signed url reverted

--- Backend/dist/utils/s3Uploader.js
+++ Backend/dist/utils/s3Uploader.js
@@ -17,7 +17,6 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const client_s3_1 = require("@aws-sdk/client-s3");
 const uuid_1 = require("uuid");
-const s3_request_presigner_1 = require("@aws-sdk/s3-request-presigner");
 const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, AWS_BUCKET_NAME, } = process.env;
 if (!AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY || !AWS_REGION || !AWS_BUCKET_NAME) {
     throw new Error('Missing AWS configuration in .env file');
@@ -29,33 +28,13 @@ const s3Client = new client_s3_1.S3Client({
         secretAccessKey: AWS_SECRET_ACCESS_KEY,
     },
 });
-// export const uploadToS3 = async (file: { buffer: Buffer; originalname: string; mimetype: string }): Promise<{ Location: string }> => {
-//   // Debug log to check file object
-//   console.log('File object:', file);
-//   const sanitizedFileName = file.originalname?.replace(/[^a-zA-Z0-9.]/g, '_') || 'default_filename';
-//   const uniqueFileName = `${uuidv4()}-${sanitizedFileName}`;;
-//   const uploadParams = {
-//     Bucket: AWS_BUCKET_NAME,
-//     Key: uniqueFileName,
-//     Body: file.buffer,
-//     ContentType: file.mimetype,
-//   };
-//   try {
-//     const command = new PutObjectCommand(uploadParams);
-//     const data = await s3Client.send(command);
-//     console.log('File uploaded successfully:', { ...data, Location: `https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com/${uploadParams.Key}` });
-//     return { Location: `https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com/${uploadParams.Key}` };
-//   } catch (error) {
-//     console.error('Error uploading file to S3:', error);
-//     throw error;
-//   }
-// };
-const uploadToS3 = (file_1, ...args_1) => __awaiter(void 0, [file_1, ...args_1], void 0, function* (file, generateSignedUrl = false // Optional parameter for signed URL
-) {
+const uploadToS3 = (file) => __awaiter(void 0, void 0, void 0, function* () {
     var _a;
+    // Debug log to check file object
     console.log('File object:', file);
     const sanitizedFileName = ((_a = file.originalname) === null || _a === void 0 ? void 0 : _a.replace(/[^a-zA-Z0-9.]/g, '_')) || 'default_filename';
     const uniqueFileName = `${(0, uuid_1.v4)()}-${sanitizedFileName}`;
+    ;
     const uploadParams = {
         Bucket: AWS_BUCKET_NAME,
         Key: uniqueFileName,
@@ -63,18 +42,10 @@ const uploadToS3 = (file_1, ...args_1) => __awaiter(void 0, [file_1, ...args_1],
         ContentType: file.mimetype,
     };
     try {
-        // Upload the file to S3
         const command = new client_s3_1.PutObjectCommand(uploadParams);
-        yield s3Client.send(command);
-        console.log('File uploaded successfully');
-        // Return Signed URL if required
-        if (generateSignedUrl) {
-            const signedUrl = yield (0, s3_request_presigner_1.getSignedUrl)(s3Client, command, { expiresIn: 3600 }); // 1-hour expiry
-            return { Location: signedUrl };
-        }
-        // Return standard S3 object URL
-        const fileUrl = `https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com/${uniqueFileName}`;
-        return { Location: fileUrl };
+        const data = yield s3Client.send(command);
+        console.log('File uploaded successfully:', Object.assign(Object.assign({}, data), { Location: `https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com/${uploadParams.Key}` }));
+        return { Location: `https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com/${uploadParams.Key}` };
     }
     catch (error) {
         console.error('Error uploading file to S3:', error);
--- Backend/src/utils/s3Uploader.ts
+++ Backend/src/utils/s3Uploader.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { v4 as uuidv4 } from 'uuid';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
 
 
 const {
@@ -24,41 +24,14 @@ const s3Client = new S3Client({
   },
 });
 
-// export const uploadToS3 = async (file: { buffer: Buffer; originalname: string; mimetype: string }): Promise<{ Location: string }> => {
+export const uploadToS3 = async (file: { buffer: Buffer; originalname: string; mimetype: string }): Promise<{ Location: string }> => {
  
-//   // Debug log to check file object
-//   console.log('File object:', file);
-
-//   const sanitizedFileName = file.originalname?.replace(/[^a-zA-Z0-9.]/g, '_') || 'default_filename';
-//   const uniqueFileName = `${uuidv4()}-${sanitizedFileName}`;;
-
-//   const uploadParams = {
-//     Bucket: AWS_BUCKET_NAME,
-//     Key: uniqueFileName,
-//     Body: file.buffer,
-//     ContentType: file.mimetype,
-//   };
-
-//   try {
-//     const command = new PutObjectCommand(uploadParams);
-//     const data = await s3Client.send(command);
-//     console.log('File uploaded successfully:', { ...data, Location: `https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com/${uploadParams.Key}` });
-//     return { Location: `https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com/${uploadParams.Key}` };
-//   } catch (error) {
-//     console.error('Error uploading file to S3:', error);
-//     throw error;
-//   }
-// };
-
-
-export const uploadToS3 = async (
-  file: { buffer: Buffer; originalname: string; mimetype: string },
-  generateSignedUrl = false // Optional parameter for signed URL
-): Promise<{ Location: string }> => {
+  
+  // Debug log to check file object
   console.log('File object:', file);
 
   const sanitizedFileName = file.originalname?.replace(/[^a-zA-Z0-9.]/g, '_') || 'default_filename';
-  const uniqueFileName = `${uuidv4()}-${sanitizedFileName}`;
+  const uniqueFileName = `${uuidv4()}-${sanitizedFileName}`;;
 
   const uploadParams = {
     Bucket: AWS_BUCKET_NAME,
@@ -68,20 +41,10 @@ export const uploadToS3 = async (
   };
 
   try {
-    // Upload the file to S3
     const command = new PutObjectCommand(uploadParams);
-    await s3Client.send(command);
-    console.log('File uploaded successfully');
-
-    // Return Signed URL if required
-    if (generateSignedUrl) {
-      const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 3600 }); // 1-hour expiry
-      return { Location: signedUrl };
-    }
-
-    // Return standard S3 object URL
-    const fileUrl = `https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com/${uniqueFileName}`;
-    return { Location: fileUrl };
+    const data = await s3Client.send(command);
+    console.log('File uploaded successfully:', { ...data, Location: `https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com/${uploadParams.Key}` });
+    return { Location: `https://${AWS_BUCKET_NAME}.s3.${AWS_REGION}.amazonaws.com/${uploadParams.Key}` };
   } catch (error) {
     console.error('Error uploading file to S3:', error);
     throw error;
