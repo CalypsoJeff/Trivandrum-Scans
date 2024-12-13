aa0747b947850cff9fc46839bdec67c8536c5ba6
Jephy Jayan Varghese <jephyjvarghese@gmail.com>
2024-12-14 00:38:53 +0530

    Update s3Uploader.js

--- frontend/src/utils/s3Uploader.js
+++ frontend/src/utils/s3Uploader.js
@@ -5,6 +5,10 @@ const REGION = import.meta.env.VITE_REGION;
 const BUCKET_NAME = import.meta.env.VITE_BUCKET_NAME;
 const ACCESS_KEY_ID = import.meta.env.VITE_ACCESS_KEY_ID;
 const SECRET_ACCESS_KEY = import.meta.env.VITE_SECRET_ACCESS_KEY;
+console.log("Region:", REGION);
+console.log("Bucket Name:", BUCKET_NAME);
+console.log("Access Key:", ACCESS_KEY_ID ? "Loaded" : "Not loaded");
+console.log("Secret Key:", SECRET_ACCESS_KEY ? "Loaded" : "Not loaded");
 
 const s3Client = new S3Client({
   region: REGION,
