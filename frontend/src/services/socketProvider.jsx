95bad8f937889e0ab3f85bf34b446638bef02cc5
Jephy Jayan Varghese <jephyjvarghese@gmail.com>
2024-12-14 01:27:59 +0530

    Update socketProvider.jsx

--- frontend/src/services/socketProvider.jsx
+++ frontend/src/services/socketProvider.jsx
@@ -54,7 +54,7 @@ function SocketProvider({ children }) {
 
   useEffect(() => {
     if (!socket) {
-      const newSocket = io("http://trivandrumscans.online");
+      const newSocket = io("https://trivandrumscans.online");
 
       newSocket.on("connect", () => {
         console.log("Socket connected:", newSocket.id);
